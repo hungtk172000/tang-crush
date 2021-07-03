2d99cd5320fbc94c65e7a4576e24441961cc3722
hungtk172000 <80811178+hungtk172000@users.noreply.github.com>
2021-07-03 09:26:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Cục Cưng à!',
+    introTitle: 'Nguyễn Thị Ngọc Anh con bố Bảy à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
